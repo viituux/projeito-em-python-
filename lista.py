52b81dfe7a1f602c82578a5e25e382f7f2356888
viituux <joaovictormarques210103@gmail.com>
2025-11-28 18:54:12 -0400

    lista

---
+++ lista.py
@@ -0,0 +1,30 @@
+# criando um lista de numeros 
+
+numeros = [1,2,3,4,5]
+print(numeros)
+
+mistos= [1,"ola",3.14,]
+print(mistos)
+
+lista_vazia = list()
+print(lista_vazia)
+
+# lista de numeros 
+lista_numeros = list([1,2,3,4,5,6,7])
+print(lista_numeros)
+
+# lista string
+
+lista_string = list("python")
+print(lista_string)
+
+# Lista  regra 
+
+lista_negra = list(range(5))
+print(lista_negra)
+
+numeros.append(6)
+print(numeros)
+
+numeros.remove(3)
+print(numeros)
